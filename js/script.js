e7b7c8ecd8a1f0c732f1421dd6d34a5e9d744e37
Bextiyar <bahtiyar_a@live.com>
2015-09-09 16:44:07 +0600

    THE FIRST EDITION

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// Make front image full paged
+function full() {
+	height = $(window).height();
+	$('header').css("height", height);
+}
+
+$(document).ready(full);
+$(window).resize(full);
+
+
+// scroll to about
+function scrolltoabout() {
+	$('html, body').animate({
+		scrollTop: $(".about").offset().top
+	}, 700);
+}
+
+$(document).ready(function() {
+	$(".scroll-down, .nav-wrapper a:nth-of-type(2)").click(scrolltoabout);
+});
+
+
+// hide and show nav bar
+function hideorshow() {
+	sd = $(".banner-wrapper").offset().top;
+	dd = $("body").scrollTop();
+	ld = $(".about").offset().top;
+	if (dd >= ld) {
+		$("nav").fadeIn(200);
+		$("nav").css("background-color", "#333333");
+	} else if (dd <= sd) {
+		$("nav").fadeIn(200);
+		$("nav").css("background-color", "rgba(0,0,0,0)");
+	} else {
+		$("nav").fadeOut(200);
+	}
+}
+
+$(document).scroll(hideorshow);
+$(window).resize(hideorshow);
+
+
+// Range sliders animation
+var done = false;
+function rangebaranimate() {
+	dd = $("body").scrollTop() + $(window).height();
+	ld = $(".up5").offset().top;
+	if (dd > ld && !done) {
+		$(".up").animate({width:"100%"});
+		done = true;
+	};
+}
+
+$(document).scroll(rangebaranimate);
+
